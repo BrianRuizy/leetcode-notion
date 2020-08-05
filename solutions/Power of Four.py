a5159fe266174c16c2dd665fa0caeb410532e678
Brian <brianruiz123@yahoo.com>
2020-08-04 22:50:09 -0500

    fixed power-of-four algo

--- solutions/Power of Four.py
+++ solutions/Power of Four.py
@@ -1,6 +1,5 @@
 from math import log
-
 class Solution:
     def isPowerOfFour(self, num: int) -> bool:        
         # True if num is not 0, AND log₄(num) is a whole-number
-        return num != 0 and log(num, 4).is_integer()
+        return num > 0 and log(num, 4).is_integer()
