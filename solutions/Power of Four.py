49d5ff812f74f9203a38b5125ec9ed863a62de02
Brian <brianruiz123@yahoo.com>
2020-08-04 22:42:50 -0500

    one liner solution for power-of-four

--- solutions/Power of Four.py
+++ solutions/Power of Four.py
@@ -1,10 +1,6 @@
 from math import log
 
 class Solution:
-    def isPowerOfFour(self, num: int) -> bool:
-        if num <= 0:
-            return False
-        
-        # if argument num is a whole number then True
-        # else not a power of 4
-        return log(num, 4).is_integer()
+    def isPowerOfFour(self, num: int) -> bool:        
+        # True if num is not 0, AND log₄(num) is a whole-number
+        return num != 0 and log(num, 4).is_integer()
