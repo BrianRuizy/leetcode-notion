cf4167f37b1d89e3e21768b34a5d06243e415542
Brian <brianruiz123@yahoo.com>
2020-08-11 02:07:06 -0500

    optimized excel sheet algorithm solution

--- solutions/week 2/Excel Sheet Column Number.py
+++ solutions/week 2/Excel Sheet Column Number.py
@@ -1,16 +1,10 @@
-from string import ascii_uppercase
-
-
 class Solution:
     def titleToNumber(self, s: str) -> int:
         total = 0
 
-        # hash letters to base 26 value
-        az_map = dict(zip(ascii_uppercase, [i for i in range(1, 27)]*26))
-
         # Example:
-        # s = 'CDA' --> az_map[C]*26*26 + az_map[D]*26 + az_map[A]
+        # s = 'CDA' --> ord(C)*26*26 + ord(D)*26 + ord(A) = 2133
         for exponent, letter in enumerate(s[::-1]):
-            total += az_map[letter]*26**exponent
+            total += (ord(letter)-64)*26**exponent
 
         return total
