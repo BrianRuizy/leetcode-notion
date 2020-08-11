76c0015fb8297c2fb29e3e68385cc032be7681bb
Brian <brianruiz123@yahoo.com>
2020-08-11 01:50:10 -0500

    improved excel sheet solution

--- solutions/week 2/Excel Sheet Column Number.py
+++ solutions/week 2/Excel Sheet Column Number.py
@@ -3,15 +3,12 @@ from string import ascii_uppercase
 
 class Solution:
     def titleToNumber(self, s: str) -> int:
-        # converting string to list
-        s = list(s)
         total = 0
 
         # hash letters to base 26 value
         az_map = dict(zip(ascii_uppercase, [i for i in range(1, 27)]*26))
 
-        # Perform base case arithmetic for each value
-        # Example: using letter for dict key
+        # Example:
         # s = 'CDA' --> az_map[C]*26*26 + az_map[D]*26 + az_map[A]
         for exponent, letter in enumerate(s[::-1]):
             total += az_map[letter]*26**exponent
