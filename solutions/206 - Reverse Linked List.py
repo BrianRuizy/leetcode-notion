ba0b9d8584d871bc676e5e8bdadaeaf17c37dbe9
Brian <brianruiz123@yahoo.com>
2020-08-23 01:41:56 -0500

    commented 206 algorithm

--- solutions/206 - Reverse Linked List.py
+++ solutions/206 - Reverse Linked List.py
@@ -5,13 +5,16 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        previous_ptr = None
-        current_ptr = head
+        """will reverse list iteratively,
+        by traversing and flipping the next pointer of each node
+        """
+        previous_ptr = None  # previous pointer, initialized to null
+        current_ptr = head  # current pointer, at head
 
         while current_ptr is not None:
-            temp = current_ptr.next
-            current_ptr.next = previous_ptr
-            previous_ptr = current_ptr
+            temp = current_ptr.next   # temp buffer, pointing to next node
+            current_ptr.next = previous_ptr  # reverse link of current_ptr to prev
+            previous_ptr = current_ptr  # ---- resume traversal ----
             current_ptr = temp
         head = previous_ptr
         return head
