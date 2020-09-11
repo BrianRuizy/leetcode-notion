a16c11c6504ce3fbd58c0b4d7f443e63b6963492
Brian <brianruiz123@yahoo.com>
2020-09-11 04:26:09 -0500

    beating 98.9% of solutions, min stack optimization

--- solutions/155 - Min Stack.py
+++ solutions/155 - Min Stack.py
@@ -1,5 +1,48 @@
-# implementation of stack data structure from scratch using linkedlist
+# ------------------------------ 2nd optimal submission -----------------------
+# Your runtime beats 98.90 % of python3 submission
+
+# 18 / 18 test cases passed.
+# Status: Accepted
+# Runtime: 52 ms
+# Memory Usage: 17.6 MB
+
+# using 2 stacks AND built-in pop(), append() methods, proved faster than
+# implementing a linked list solution ??
+
+class MinStack2:
+    def __init__(self):
+        self.stack = []
+        self.smallest = [9223372036854775807]
+
+    def pop(self):
+        current = self.stack[-1]
+        if current == self.smallest[-1]:
+            self.smallest.pop()
+        self.stack.pop()
+
+    def push(self, x):
+        if x <= self.smallest[-1]:
+            self.smallest.append(x)
+        self.stack.append(x)
+
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.smallest[-1]
+
+# ------------------------------ 1st optimized submission ---------------------
+# Your runtime beats 62.03 % of python3 submissions
+
+# 18 / 18 test cases passed.
+# Status: Accepted
+# Runtime: 68 ms
+# Memory Usage: 18.4 MB
+
+# implementation of stack data-structure from scratch using linkedlist
 # many solutions I seen use a regular [] list with built in pop(), append() methods
+
+
 class Node:
     def __init__(self, data=None, next=None):
         self.data = data
